4ed84c4f6d58246440f91dba3f08293c20360dbe
rocket0xdev1025 <rocket0xdev@gmail.com>
2026-04-22 02:35:32 +0900

    first commit

---
+++ api/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+
+app.get('/api/generated-images', async (req, res) => {
+
+  try {
+    const response = await fetch('https://www.makealiensgreatagain.com/api/generated-images');
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    console.error('Error fetching data:', err);
+    res.status(500).json({
+      error: 'Failed to retrieve data'
+    });
+  }
+});
+
+
+app.get('/validators', async (req, res) => {
+  try {
+    const {
+      task_id,
+      size,
+      page
+    } = req.query;
+
+    if (!task_id) {
+      return res.status(400).json({
+        error: 'task_id is required'
+      });
+    }
+    const response = await fetch(`https://fed-ledger-prod.flock.io/api/v1/stats/validators?task_id=${task_id}&page=${page}&size=${size}`);
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    console.error('Error fetching data:', err);
+    res.status(500).json({
+      error: 'Failed to retrieve data'
+    });
+  }
+});
+
+
+module.exports = app;
